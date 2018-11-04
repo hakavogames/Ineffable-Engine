844eaea9fc3c9476c8d258c3565041e460ff654a
hakavogames <hakavogames@gmail.com>
2018-11-04 21:21:51 +0200

    Fixed project misconfiguration

--- Ineffable-Engine/desktop/src/com/kozma/desktop/DesktopLauncher.java
+++ Ineffable-Engine/desktop/src/com/kozma/desktop/DesktopLauncher.java
@@ -2,15 +2,19 @@ package com.kozma.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.hakavo.Game;
+import com.hakavo.game.Game;
+import java.io.IOException;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main (String[] arg) throws IOException {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
                 config.width=1600;
                 config.height=900;
+                config.fullscreen=false;
                 config.useGL30=true;
+                config.foregroundFPS=10000;
                 config.allowSoftwareMode=false;
+                config.samples=16;
                 config.vSyncEnabled=true;
 		new LwjglApplication(new Game(), config);
 	}
